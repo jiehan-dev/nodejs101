2f45b78519e7d05984905aee1e4d065363e04133
Lim Jie Han <jiehan.develop@gmail.com>
2020-02-01 14:42:45 +0800

    complete

---
+++ 5-web/1-http/1-helloworld.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const requestListener = (req, res) => {
+  // req, res are streams!
+  console.dir(req, { depth: 0 });
+  console.dir(res, { depth: 0 });
+  res.end("Hello World\n");
+
+  // equivalent to
+  // res.write("Hello World\n");
+  // res.end();
+};
+
+const server = http.createServer(requestListener);
+
+// or we can do it this way
+// const server = http.createServer();
+// server.on("request", requestListener);
+
+server.listen(4242, () => {
+  console.log("Server is running...");
+});
