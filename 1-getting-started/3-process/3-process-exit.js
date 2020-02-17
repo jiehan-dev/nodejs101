a69a7ef36f7e49fadb2afb63361f13102ddf7af9
Jiehan <jiehan.develop@gmail.com>
2020-02-17 18:31:33 +0800

    minor

--- 1-getting-started/3-process/3-process-exit.js
+++ 1-getting-started/3-process/3-process-exit.js
@@ -1,7 +1,11 @@
 setTimeout(() => process.exit(), 2000);
 
+// process is an event emitter
 process.on('exit', () => {
   console.log('Process will exit now. See you later!');
+
+  // do one final synchronous operation
+  // before the node process terminates
 });
 
 console.log('Hello!');
